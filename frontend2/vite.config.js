5727f7af853372a9d33d0cd08144d0ce288700b8
Abhishek <224abhishekjha@gmail.com>
2024-07-19 23:50:42 +0100

    Final commit before

--- frontend2/vite.config.js
+++ frontend2/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: '"https://gymwebsitemain-2.onrender.coms',
+        target: 'https://gymwebsitemain-2.onrender.com',
         changeOrigin: true,
       },
     },
