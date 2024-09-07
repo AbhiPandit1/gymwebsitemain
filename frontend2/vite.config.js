7fb0af1862e71b1d8c4813d2267709bba439fd71
Abhishek <224abhishekjha@gmail.com>
2024-09-08 04:52:32 +0530

    changed link

--- frontend2/vite.config.js
+++ frontend2/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       // Proxy API requests to a backend server during development
       '/api': {
-        target: 'http://localhost:3000', // Backend server URL
+        target: 'https://gymwebsitemain-2.onrender.com', // Backend server URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // Optional: rewrite path if needed
       },
