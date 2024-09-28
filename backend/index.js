c9d00cf79432f4b2782a54a74cce788179ae773f
Abhishek <224abhishekjha@gmail.com>
2024-09-28 05:54:05 +0530

    Corrected something

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post(
-  '/account-webhook',
+  '/webhook/account',
   express.raw({ type: 'application/json' }),
   stripeWebhook
 );
